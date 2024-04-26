a9aaa6bea3be68c713cde1204b9be861e6eb6f02
Arcoafrin <161829930+Arcoafrin@users.noreply.github.com>
2024-04-26 16:53:39 +0530

    Initial commit

---
+++ src/Heading.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function Heading() {
+  return <h1>Javascript course content</h1>;
+}
+export default Heading;
---
+++ src/List.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+function List() {
+  return (
+    <div>
+      <ul>
+        <li>Variables & data types.</li>
+        <li>Operators & conditional statements.</li>
+        <li>Loops & string.</li>
+        <li>Arrays.</li>
+        <li>Functions &Methods.</li>
+        <li>DOM (document object model).</li>
+        <li>Events.</li>
+        <li>Promises.</li>
+        <li>Async Await</li>
+      </ul>
+    </div>
+  );
+}
+export default List;
